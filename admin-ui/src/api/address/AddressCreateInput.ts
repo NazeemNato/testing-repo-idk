2e69e7c88358b4fc90fe0a297c41acc7d88be987
Muhammed Nazeem <39720973+NazeemNato@users.noreply.github.com>
2021-11-20 13:26:57 +0530

    amplication-build-ckw7ipm5x5445511bs65dwtdwah

---
+++ admin-ui/src/api/address/AddressCreateInput.ts
@@ -0,0 +1,8 @@
+export type AddressCreateInput = {
+  address_1?: string | null;
+  address_2?: string | null;
+  city?: string | null;
+  email: string;
+  state?: string | null;
+  zip?: number | null;
+};
